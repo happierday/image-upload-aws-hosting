4b5e2928a8f092d90721b64ab727e8d980f937be
jian <jianchengw26@gmail.com>
2017-10-23 19:04:10 -0400

    restart causing new public DNS

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
     secret: crypto,
-    url : 'mongodb://wu134679:wu134679@ec2-18-216-60-167.us-east-2.compute.amazonaws.com/dummyDB'
+    url : 'mongodb://wu134679:wu134679@ec2-18-216-51-130.us-east-2.compute.amazonaws.com/dummyDB'
 }
